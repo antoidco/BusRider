3af0116396195e6b847a0bd6fdb702912e2a73d6
antoidco <antoidco@gmail.com>
2020-10-24 16:32:03 +0300

    Photon tutorial part 3, gamescenes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+namespace Com.Antoid.Bus {
+    public class GameManager : MonoBehaviourPunCallbacks {
+        #region Photon Callbacks
+        
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public override void OnLeftRoom() {
+            SceneManager.LoadScene(0); // really ?
+        }
+
+        #endregion
+
+
+        #region Public Methods
+
+        public void LeaveRoom() {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+    }
+}
