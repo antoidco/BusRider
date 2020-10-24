b0613cf18268aa3369b208c34f138afffb829c93
antoidco <antoidco@gmail.com>
2020-10-25 01:16:06 +0300

    Photon tutorial part 5, player-prefab (mostly skip, i use bus instead)

---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Com.Antoid.Bus
+{
+    public class PlayerAnimatorManager : MonoBehaviour
+    {
+        private Animator _animator;
+        #region MonoBehaviour Callbacks
+
+
+        // Use this for initialization
+        void Start() {
+            _animator = GetComponent<Animator>();
+            if (!_animator)
+                Debug.Log("Animator is missing in PlayerAnimatorManager");
+        }
+
+
+        // Update is called once per frame
+        void Update() {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            if (v < 0) {
+                v = 0;
+            }
+            
+            if (_animator != null)
+                _animator.SetFloat("Speed", h * h + v * v);
+        }
+
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/PlayerBusController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Com.Antoid.Bus {
+    public class PlayerBusController : MonoBehaviour {
+        private Rigidbody _body;
+        public float horzPower = 1f;
+        public float vertPower = 5f;
+        private float _horzInput = 0f;
+        private float _vertInput = 0f;
+        
+        private void Start() {
+            _body = GetComponent<Rigidbody>();
+        }
+        
+        // Update is called once per frame
+        private void Update() {
+            _horzInput = Input.GetAxis("Horizontal");
+            _vertInput = Input.GetAxis("Vertical");
+        }
+        
+        private void FixedUpdate() {
+            var force = new Vector3(0, 0, _vertInput * vertPower);
+            var tourqe = new Vector3(0, _horzInput * horzPower, 0);
+            _body.AddForce(transform.rotation * force);
+            _body.AddTorque(transform.rotation * tourqe);
+        }
+    }
+}
