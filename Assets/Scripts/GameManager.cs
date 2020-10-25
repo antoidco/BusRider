df7cb07bbdca48b1f1b2e1d08ab3a0b774388c4b
antoidco <antoidco@gmail.com>
2020-10-25 15:32:39 +0300

    Photon tutorial part 8, instantiating

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,10 @@ using Photon.Realtime;
 
 namespace Com.Antoid.Bus {
     public class GameManager : MonoBehaviourPunCallbacks {
+        
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+        
         #region Photon Callbacks
         
         /// <summary>
@@ -59,5 +63,21 @@ namespace Com.Antoid.Bus {
         }
 
         #endregion
+        
+        
+        private void Start() {
+            if (playerPrefab == null) {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",this);
+            }
+            else {
+                if (PlayerManager.LocalPlayerInstance == null) {
+                    Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+                    // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f,5f,0f), Quaternion.identity, 0);
+                } else {
+                    Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -8,6 +8,19 @@ namespace Com.Antoid.Bus {
     /// Player manager.
     /// </summary>
     public class PlayerManager : MonoBehaviourPunCallbacks {
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
+        private void Awake() {
+            // #Important
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+            if (photonView.IsMine) {
+                PlayerManager.LocalPlayerInstance = this.gameObject;
+            }
+            // #Critical
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
+        }
 
         private void Start() {
             CameraWork _cameraWork = this.gameObject.GetComponent<CameraWork>();
